44d257525393695098f8d7ffb70fba568d7762dc
Kutalmis Coskun <kutalmis.coskun@uni-rostock.de>
2025-09-22 12:29:28 +0000

    implement a linear version of EMC referred to as EMCLin

--- src/emc/estimator/EMC.py
+++ src/emc/estimator/EMC.py
@@ -1,5 +1,7 @@
+import copy
 import logging
 from collections import deque
+from itertools import product
 
 import numpy as np
 from tqdm import tqdm
@@ -247,3 +249,352 @@ class EMC:
 		else:
 			for symbol in sequence:
 				self.process_symbol(symbol)
+
+class EMCLin:
+
+    def __init__(
+        self,
+        alpha, # int, alphabet cardinality, it is needed to initialize the estimate tensor
+        order, # int, Markov order of the chain
+        lambda_, # list or double [0,1], AKA: learning coefficient
+        beta, # double [0,1], entropy regularization rate
+        delta, # list or double [0,1], min distance to fire drift detection
+        eta, # list or double [0,1], model similarity threshold
+        tau, # int, distance between compared estimates in terms of symbols
+        live_memory=True, # boolean, whether to update models in memory
+        symbols_are_indices=True, # boolean, whether to use symbols as indices in estimate tensor
+        log_file_path=None # path, file to write logs
+    ):
+
+        # initialize EMC instance
+        self.alpha = alpha
+        self.order = order
+        self.lambda_fast, self.lambda_slow = lambda_[:2] if isinstance(lambda_, list) else (lambda_, lambda_)
+        self.beta = beta
+        self.delta_fast, self.delta_slow = delta[:2] if isinstance(delta, list) else (delta, delta)
+        self.eta_fast, self.eta_slow = eta[:2] if isinstance(eta, list) else (eta, eta)
+        self.tau = tau
+
+        # settings
+        self.live_memory = live_memory
+        self.symbols_are_indices = symbols_are_indices
+        self.is_stationary = False # start from non-stationary state
+        self.dist = hellinger_distance
+        self.obs_count = 0
+
+        # estimate and prediction histories
+        self.P = {}
+        self.P_ref = {}
+        self.P_exp = self.P
+        self.updated_context_keys = []
+        self.distances_to_modes = {}
+
+        self.uniform_cpd = np.full(shape=self.alpha, fill_value=1/self.alpha, dtype=np.float64)
+        self.all_context_keys = product(*[range(self.alpha)] * self.order)
+
+        self.curr_mode_pred = None # id of the most recently predicted mode
+        self.pred_mode_hist = [] # predicted mode history
+
+        # symbol to index and index to symbol maps
+        self.symbol_index_map = {}
+        self.index_symbol_map = {}
+
+        # condition vectors (symbol and index)
+        self.cnd_vec = deque(maxlen=self.order)
+        self.cndi_vec = deque(maxlen=self.order)
+
+        # detections
+        self.detected_drifts = []
+        self.detected_changes = []
+        self.drift_scores = []
+        self.stationarity_history = []
+
+        # the memory
+        self.learned_modes = {}
+        self.n_memory_updates = {}
+
+    # manages symbol->index and index->symbol mappings
+    # - assigns a new index to given symbol if it has not been observed before
+    # - returns index
+    def get_index(self, symbol):
+        if self.symbols_are_indices:
+            if symbol not in self.symbol_index_map:
+                self.symbol_index_map[symbol] = symbol
+                self.index_symbol_map[symbol] = symbol
+            index = symbol
+        else:
+            if len(self.symbol_index_map) == 0:
+                self.symbol_index_map[symbol] = 0
+                self.index_symbol_map[0] = symbol
+            elif symbol not in self.symbol_index_map:
+                if len(self.symbol_index_map) >= self.alpha:
+                    print(f"Unexpected symbol observed: {symbol}")
+                    exit()
+                next_available_index = max(self.symbol_index_map.values()) + 1
+                self.symbol_index_map[symbol] = next_available_index
+                self.index_symbol_map[next_available_index] = symbol
+            index = self.symbol_index_map[symbol]
+        return index
+
+    def update_reference(self, update_type="partial"):
+
+        # full copy
+        if update_type == "full":
+            self.P_ref = {key: np.copy(value) for key, value in self.P.items()}
+
+        # partial copy
+        elif update_type == "partial":
+            for key in self.updated_context_keys:
+                self.P_ref[key] = np.copy(self.P[key])
+
+        # deep copy
+        else:
+            self.P_ref = copy.deepcopy(self.P)
+
+        # reset
+        self.updated_context_keys = []
+
+    def update_estimate(self, symbol_index):
+
+        # get lambda depending on stationarity
+        lambda_ = self.lambda_slow if self.is_stationary else self.lambda_fast
+
+        # get the key for the current condition vector
+        key = tuple(self.cndi_vec)
+
+        # update P
+        if key not in self.P:
+            self.P[key] = np.full(shape=self.alpha, fill_value=1/self.alpha, dtype=np.float64)
+        self.P[key] *= lambda_
+        self.P[key][symbol_index] += (1 - lambda_)
+
+        # keep track of indices of updated CPDs
+        if key not in self.updated_context_keys:
+            self.updated_context_keys.append(key)
+
+    def entropy_regularization(self, type="partial"):
+
+        # update all contexts that exist in P
+        if type == "full":
+            for k, v in self.P.items():
+                if k == tuple(self.cndi_vec):
+                    continue
+                self.P[k] = v * (1 - self.beta) + self.uniform_cpd * self.beta
+
+        # update all contexts that have not been updated in current batch and exist in P
+        elif type == "partial":
+            for context_key in self.all_context_keys:
+                if context_key in self.updated_context_keys or context_key not in self.P:
+                    continue
+                self.P[context_key] = self.P[context_key] * (1 - self.beta) + self.uniform_cpd * self.beta
+
+    # compute the distance between P and P_ref
+    def compute_drift_score(self):
+
+        drift_score = 0
+        for context_key in self.updated_context_keys:
+            if context_key in self.P_ref:
+                cpd_dist = self.dist(self.P[context_key], self.P_ref[context_key])
+            else:
+                cpd_dist = self.dist(self.P[context_key], self.uniform_cpd)
+            if cpd_dist > drift_score:
+                drift_score = cpd_dist
+        self.drift_scores.append(drift_score)
+
+    # compute the distance between P_ref and learned modes
+    def compute_mode_distances(self):
+
+        if len(self.learned_modes) > 0:
+
+            for mode_id, mode_repr in self.learned_modes.items():
+                max_context_dist = 0
+                max_context_key = None
+                for context_key in self.updated_context_keys:
+                    if context_key in mode_repr:
+                        if context_key not in self.P:
+                            print(f"{self.obs_count} Context key {context_key} not found in P")
+                        context_dist = self.dist(self.P[context_key], mode_repr[context_key].mean)
+                    else:
+                        context_dist = self.dist(self.P[context_key], self.uniform_cpd)
+                    if context_dist > max_context_dist:
+                        max_context_dist = context_dist
+                        max_context_key = context_key
+                self.distances_to_modes[mode_id] = (max_context_key, max_context_dist)
+
+        # print(f"Mode distances ({len(self.learned_modes)}) at {self.obs_count}: {self.distances_to_modes}")
+
+    def check_drift(self):
+
+        if self.is_stationary:
+
+            # S -> NS
+            if self.drift_scores[-1] > self.delta_slow:
+                self.detected_drifts.append(self.obs_count)
+                self.is_stationary = False
+                self.predict_regime()
+                # print(f"Drift detected at {self.obs_count} with score {self.cpd_dist_max:.4f}")
+            
+            # S -> S
+            else:
+                pass
+
+        else:
+
+            # NS -> NS
+            if self.drift_scores[-1] > self.delta_fast:
+                self.predict_regime()
+                # pass
+
+            # NS -> S
+            else:
+                self.is_stationary = True
+                self.predict_regime()
+                # print(f"Stationarity detected at {self.obs_count} with score {self.cpd_dist_max:.4f}")
+
+    def save_mode(self):
+        new_mode_id = len(self.learned_modes)+1
+        self.learned_modes[new_mode_id] = {key: ExpectedTensor(copy.deepcopy(value)) for key, value in self.P.items()}
+        self.curr_mode_pred = new_mode_id
+        # print(f"Mode {new_mode_id} saved at {self.obs_count}")
+
+    # live memory update
+    def update_mode(self):
+         
+        context_key = tuple(self.cndi_vec)
+        if context_key not in self.learned_modes[self.curr_mode_pred]:
+            uniform_cpd = np.full(shape=self.alpha, fill_value=1/self.alpha, dtype=np.float64)
+            self.learned_modes[self.curr_mode_pred][context_key] = ExpectedTensor(initial_tensor=uniform_cpd)
+        if context_key in self.P:
+            val = self.P[context_key]
+            self.learned_modes[self.curr_mode_pred][context_key].update(val)
+
+        if self.curr_mode_pred in self.distances_to_modes:
+            known_max_context_dist = self.distances_to_modes[self.curr_mode_pred][1]
+            if context_key in self.learned_modes[self.curr_mode_pred]:
+                context_dist = self.dist(self.P[context_key], self.learned_modes[self.curr_mode_pred][context_key].mean)
+            else:
+                context_dist = self.dist(self.P[context_key], self.uniform_cpd)
+            if context_dist > known_max_context_dist:
+                self.distances_to_modes[self.curr_mode_pred] = (context_key, context_dist)
+
+    def check_memory(self):
+
+        mode_dists = {}
+        for mode_id, mode_repr in self.learned_modes.items():
+
+            # prepare a list of context keys to check
+            context_keys_to_check = self.updated_context_keys.copy()
+            if mode_id in self.distances_to_modes:
+                max_context_key = self.distances_to_modes[mode_id][0]
+                if max_context_key not in context_keys_to_check and max_context_key is not None:
+                    context_keys_to_check.append(max_context_key)
+
+            # compute the maximum context distance
+            max_context_dist = 0
+            for context_key in context_keys_to_check:
+                if context_key in mode_repr:
+                    context_dist = self.dist(self.P[context_key], mode_repr[context_key].mean)
+                else:
+                    context_dist = self.dist(self.P[context_key], self.uniform_cpd)
+                if context_dist > max_context_dist:
+                    max_context_dist = context_dist
+            mode_dists[mode_id] = max_context_dist
+
+        return dict(sorted(mode_dists.items(), key=lambda item: item[1]))
+
+    def predict_regime(self):
+        
+        # mode memory is empty
+        if len(self.learned_modes) == 0:
+            
+            # save the mode
+            if self.is_stationary:
+                self.save_mode()
+                # print(f"EMCF: Mode {self.curr_mode_pred} saved at {self.obs_count}")
+
+        # mode memory is not empty
+        else:
+
+            closest_model_id = min(self.distances_to_modes, key=lambda k: self.distances_to_modes[k][1])
+            min_distance = self.distances_to_modes[closest_model_id][1]
+
+            # closest model is sufficiently close
+            eta = self.eta_slow if self.is_stationary else self.eta_fast
+            if min_distance < eta:
+                self.curr_mode_pred = closest_model_id
+                # print(f"Mode {closest_model_id} predicted at {self.obs_count} with distance {min_distance:.4f}")
+
+            # closest model is not sufficiently close
+            else:
+
+                # print(f"No close mode found at {self.obs_count} with distance {min_distance:.4f}")
+
+                # save the mode
+                if self.is_stationary:
+                    self.save_mode()
+                    # print(f"EMCF: Mode {self.curr_mode_pred} saved at {self.obs_count}, d:{min_distance:.4f}, cm:{closest_model_id}")
+
+    # processes a single symbol
+    def process_symbol(self, symbol):
+
+        index = self.get_index(symbol)
+
+        if len(self.cndi_vec) == self.order:
+
+            # learn
+            self.update_estimate(index)
+
+            # entropy regularization
+            if self.beta > 0:
+                self.entropy_regularization(type="full")
+
+            if self.obs_count % self.tau == 0:
+                self.compute_drift_score()
+                self.compute_mode_distances()
+                self.check_drift()
+                self.update_reference()
+
+            if self.is_stationary and self.live_memory:
+                self.update_mode()
+
+        # maintain memory
+        if len(self.learned_modes) == 0:
+            self.pred_mode_hist.append(1)
+            self.P_exp = self.P.copy()
+
+        else:
+
+            # a change is detected if the current mode prediction is different from the previous one
+            if self.curr_mode_pred != self.pred_mode_hist[-1]:
+                self.detected_changes.append(self.obs_count)
+
+            # append the current mode prediction to the history
+            self.pred_mode_hist.append(self.curr_mode_pred)
+
+            # update the expected predictions
+            if self.is_stationary:
+                self.P_exp = self.learned_modes[self.curr_mode_pred].copy()
+            else:
+                self.P_exp = self.P.copy()
+
+        self.stationarity_history.append([0,1][self.is_stationary])
+        self.obs_count += 1
+        self.cnd_vec.append(symbol)
+        self.cndi_vec.append(index)
+
+    # processes each symbol in a given sequence, shows progress information
+    def process_sequence(self, sequence, progress=False, progress_desc="", progress_desc_width=8):
+        if progress:
+            pbar_conf = {
+                "total": len(sequence),
+                "desc": progress_desc.ljust(progress_desc_width),
+                "bar_format": "{l_bar}{bar:20}{r_bar}{bar:-20b}",
+                "ascii": True
+            }
+            with tqdm(**pbar_conf) as pbar:
+                for symbol in sequence:
+                    self.process_symbol(symbol)
+                    pbar.update()
+        else:
+            for symbol in sequence:
+                self.process_symbol(symbol)
